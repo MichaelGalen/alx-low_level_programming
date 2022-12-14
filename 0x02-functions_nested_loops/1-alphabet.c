e2990cba841079849ebbff5deb3e1a3b911d6823
MichaelGalen <adekogamichael9@gmail.com>
2022-12-13 21:00:30 -0800

    Task 1 - 8

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabeth - funtion to print abc
+ *
+ * Return: 0
+ */
+void print_alphabeth(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * jack bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	 for (hour = 0; hour <= 23; hour++)
+	 {
+		 for (minute = 0; minite <= 59; minute++)
+	 }
+	 _putchar((hou / 10) + '0');
+	 _putchar((hour
+
+
