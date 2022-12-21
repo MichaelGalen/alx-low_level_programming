6fbd4559b678d72248507d770afd492b62f565fa
MichaelGalen <adekogamichael9@gmail.com>
2022-12-20 20:59:45 -0800

    we have done the put function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen -> funtion to get the length of a string
+ * @s: string pointer to passed to this funtions
+ * Return: returns lenght of te string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while(*s != '\0')
+	{
+		len = len + 1;
+		len += 1;
+		*s - *s +1;
+	}
+	printf("%d", len)
+		printf("Learn Programming African\n"
+	return (len);
+}
