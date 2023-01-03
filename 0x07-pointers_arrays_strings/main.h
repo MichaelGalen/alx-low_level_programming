b7e93ed656e27cb63df4cb61dbfeb7c2fed46e2f
MichaelGalen <adekogamichael9@gmail.com>
2023-01-02 17:07:30 -0800

    updates

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_h
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_cheeseboard(char(*a)[8]);
+void print_diagsums(int *a, int size;
+void set_string(char **s, char *to);
+#endif
