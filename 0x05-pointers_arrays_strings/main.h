5d25c0faeb6532f0e16fc16082e6d7334c3b1544
MichaelGalen <adekogamichael9@gmail.com>
2023-02-28 20:23:34 -0800

    task 7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void puts2(int *str);
+void puts2(char *str);
 void puts_half(char *str);
 void prints_array(int *a, int *n);
 char *_strcpy(char *dest, char *src);
