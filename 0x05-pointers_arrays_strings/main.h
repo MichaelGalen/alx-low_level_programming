0dca79209a9f6b2069cc4d8dc91a44798db5dec8
MichaelGalen <adekogamichael9@gmail.com>
2023-02-28 18:09:58 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2(int *str);
+void puts_half(char *str);
+void prints_array(int *a, int *n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+
+#endif
